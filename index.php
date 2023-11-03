e93f95e09011fada062117a53eda361d0b997a5f
Daanielaih <daniela.perez4@mail.udp.cl>
2023-11-03 01:34:08 -0300

    br homerin

--- index.php
+++ index.php
@@ -14,6 +14,8 @@
         </header>
         <!-- About section-->
         <div class="text-center"><img src="assets/homero2.jpg"></div>
+        </br>
+
         <!-- Footer-->
         <footer class="py-5 bg-dark">
             <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; Popos unidos 2023</p></div>
