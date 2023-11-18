136bfa8c47ab0fe9f4ae0b9edce9e8f8f9fc25ba
Daanielaih <daniela.perez4@mail.udp.cl>
2023-11-18 01:02:33 -0300

    server.js

--- mongo.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-mongoose.connect(http://3.137.186.179/, {useNewUrlParser: true, useUnifiedTopology: true});
-
-// Verificar la conexión
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'Error de conexión a MongoDB:'));
-db.once('open', function() {
-  console.log('Conexión exitosa a MongoDB');
-});
